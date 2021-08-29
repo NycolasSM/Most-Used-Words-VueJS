e67daee3d6c735d23479de203567a9da58bd09df
Nycolas Santana <nycolassantana00@gmail.com>
2021-08-29 09:39:27 -0300

    first-commit

---
+++ src/backend/index.js
@@ -0,0 +1,14 @@
+const { ipcMain } = require('electron')
+
+const pathsToRows = require('./pathsToRows')
+const prepareData = require('./prepareData')
+const groupedWords = require('./groupWords')
+
+ipcMain.on('process-subtitles', (event, paths) => {
+  console.log(paths)
+
+  pathsToRows(paths)
+    .then(rows => prepareData(rows))
+    .then(words => groupedWords(words))
+    .then(groupedWords => event.reply('process-subtitles', groupedWords))
+})
